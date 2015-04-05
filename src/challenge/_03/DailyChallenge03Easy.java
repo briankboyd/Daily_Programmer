c72eedbad30786803c111953b85888affc35d6e5
Brian Boyd <bboyd019@yahoo.com>
2015-04-05 18:41:02 -0500

    init challenge 03

---
+++ src/challenge/_03/DailyChallenge03Easy.java
@@ -0,0 +1,14 @@
+package challenge._03;
+
+public class DailyChallenge03Easy {
+	public static void main(String[] args) {
+		
+		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+		
+	}
+	
+	public static void ceaserCipherEncrypt() {
+		
+	}
+	
+}
