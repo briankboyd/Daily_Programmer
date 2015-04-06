f249feaf5b2d0b2e3e516279d518cbf60dd70d91
Brian Boyd <bboyd019@yahoo.com>
2015-04-05 20:01:12 -0500

    Created cipher function, still needs some work

--- src/challenge/_03/DailyChallenge03Easy.java
+++ src/challenge/_03/DailyChallenge03Easy.java
@@ -1,14 +1,36 @@
 package challenge._03;
 
+import java.util.Scanner;
+
 public class DailyChallenge03Easy {
+	
+	final private static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+	private final static int key = 1;
+	
 	public static void main(String[] args) {
 		
-		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-		
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter some text to be securityfied.");
+		String plainText = kb.nextLine().toLowerCase();
+		kb.close(); 
+		String cipherText = ceaserCipherEncrypt(key, plainText, alphabet);
+		System.out.println(cipherText.toUpperCase());
 	}
 	
-	public static void ceaserCipherEncrypt() {
+	public static String  ceaserCipherEncrypt(int key, String plainText, char[] alphabet) {
+
+		char[] plain = plainText.toCharArray();
+		String cipherText = "";
 		
+		for (int j = 0; j < plain.length; j++) {
+				
+			for (int i = 0; i < alphabet.length; i++) {
+				if(plain[j] == alphabet[i]) {
+					cipherText += alphabet[i + key];
+				}
+			}
+		}
+		return cipherText;
 	}
 	
 }
--- src/challenge/_03/README.md
+++ src/challenge/_03/README.md
@@ -12,6 +12,5 @@ for extra credit, add a "decrypt" function to your program!
 
 
 ###Status: Incomplete
-
-
-
+* Need to add decrypt function
+* Need to handle shifting the array out of bounds, if input is z and shifts to the right 1, the array should goto the beginning of the array, to a
