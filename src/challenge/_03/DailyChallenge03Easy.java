8831a416834a2517eb377515769d48e61e8573db
Brian Boyd <bboyd019@yahoo.com>
2015-04-05 23:05:56 -0500

    added decrypt function and fixed out of bounds error on the cipher function

--- src/challenge/_03/DailyChallenge03Easy.java
+++ src/challenge/_03/DailyChallenge03Easy.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class DailyChallenge03Easy {
 	
 	final private static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-	private final static int key = 1;
+	private final static int key = 2;
 	
 	public static void main(String[] args) {
 		
@@ -15,6 +15,7 @@ public class DailyChallenge03Easy {
 		kb.close(); 
 		String cipherText = ceaserCipherEncrypt(key, plainText, alphabet);
 		System.out.println(cipherText.toUpperCase());
+		System.out.println(ceaserCipherDecrypt(key, cipherText, alphabet));
 	}
 	
 	public static String  ceaserCipherEncrypt(int key, String plainText, char[] alphabet) {
@@ -23,14 +24,35 @@ public class DailyChallenge03Easy {
 		String cipherText = "";
 		
 		for (int j = 0; j < plain.length; j++) {
-				
 			for (int i = 0; i < alphabet.length; i++) {
 				if(plain[j] == alphabet[i]) {
-					cipherText += alphabet[i + key];
+					if(i + key > 25) {
+						cipherText += alphabet[(i + key) - 26];
+					}else {
+						cipherText += alphabet[i + key];	
+					}
 				}
 			}
 		}
 		return cipherText;
 	}
 	
+	public static String ceaserCipherDecrypt(int key, String cipherText, char[] alphabet) {
+		
+		char[] cipher = cipherText.toCharArray();
+		String plainText = "";
+		
+		for (int j = 0; j < cipher.length; j++) {
+			for (int i = 0; i < alphabet.length; i++) {
+				if(cipher[j] == alphabet[i]) {
+					if(i - key < 0) {
+						plainText += alphabet[(i - key) + 26];
+					}else {
+						plainText += alphabet[i - key];	
+					}
+				}
+			}
+		}
+		return plainText;
+	}
 }
--- src/challenge/_03/README.md
+++ src/challenge/_03/README.md
@@ -11,6 +11,7 @@ write a program that can encrypt texts with an alphabetical caesar cipher. This 
 for extra credit, add a "decrypt" function to your program!
 
 
-###Status: Incomplete
+###Status: Complete
 * Need to add decrypt function
 * Need to handle shifting the array out of bounds, if input is z and shifts to the right 1, the array should goto the beginning of the array, to a
+* Added  decrypt function
