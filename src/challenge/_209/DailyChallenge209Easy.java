44b6d48254d1da16f224564cab9f818b5b91611b
Brian Boyd <bboyd019@yahoo.com>
2015-04-08 21:30:49 -0500

    Adding data to be used for challenge 209, think I understand what I've got to fo now.

--- src/challenge/_209/DailyChallenge209Easy.java
+++ src/challenge/_209/DailyChallenge209Easy.java
@@ -3,6 +3,16 @@ package challenge._209;
 public class DailyChallenge209Easy {
 	public static void main(String[] args) {
 		
-		
 	}
+}
+
+class usersTime {
+	
+	bholzer: 101.09
+	Cosmologicon: 27.45
+	nint22: 13.76
+	nooodl: 7.29
+	nottoobadguy: 74.56
+	oskar_s: 39.90
+	Steve132: 61.82
 }
